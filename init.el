100f98408de7689df0a1bdb6d7bb7cd6df2da62b
gpapadok <38889721+gpapadok@users.noreply.github.com>
2022-12-08 17:44:12 +0200

    Update init.el

--- init.el
+++ init.el
@@ -1,28 +1,54 @@
 (require 'package)
 (add-to-list 'package-archives
-	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
-;; (package-refresh-contents)
+
+(add-to-list 'load-path "~/.emacs.d/rainbow")
 
 ;; Download evil
-;; (unless (package-installed-p 'evil)
-;;   (package-install 'evil))
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
 
-;; (setq evil-want-C-u-scroll t)
-;; (require 'evil)
-;; (evil-mode 1)
+(setq evil-want-C-u-scroll t)
+(require 'evil)
+(evil-mode 1)
 
+(require 'rainbow-delimiters)
+(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+
+(add-hook 'clojure-mode-hook 'display-line-numbers-mode)
+(add-hook 'clojure-mode-hook 'paredit-mode)
+
+(setq-default truncate-lines 1)
+(setq display-line-numbers-type 'relative)
+
+(setq js-indent-level 2)
+
+(show-paren-mode 1)
 (column-number-mode 1)
 (line-number-mode 1)
-(display-line-numbers-mode 1)
-(setq display-line-numbers-type 'relative)
+
+;; (setq tab-always-indent 'complete)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(cider clojure-mode cmake-mode)))
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
+ '(custom-enabled-themes '(manoj-dark))
+ '(ispell-dictionary nil)
+ '(package-selected-packages '(deadgrep cider clojure-mode cmake-mode helm))
+ '(safe-local-variable-values
+   '((cider-figwheel-main-default-options . ":dev")
+     (cider-default-cljs-repl . figwheel-main)
+     (cider-clojure-cli-global-options . "-A:dev")))
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
