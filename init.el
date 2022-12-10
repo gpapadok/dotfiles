13dcc442d419b4e7c8103f1216c2521c2b623fab
gpapadok <38889721+gpapadok@users.noreply.github.com>
2022-12-10 15:42:22 +0200

    Update init.el

--- init.el
+++ init.el
@@ -3,30 +3,20 @@
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
 
-(add-to-list 'load-path "~/.emacs.d/rainbow")
-
-;; Download evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
-
-(setq evil-want-C-u-scroll t)
-(require 'evil)
-(evil-mode 1)
-
-(require 'rainbow-delimiters)
-(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
-
-(add-hook 'clojure-mode-hook 'display-line-numbers-mode)
-(add-hook 'clojure-mode-hook 'paredit-mode)
-
-(setq-default truncate-lines 1)
-(setq display-line-numbers-type 'relative)
-
-(setq js-indent-level 2)
-
-(show-paren-mode 1)
-(column-number-mode 1)
-(line-number-mode 1)
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)                                                                                                                           
+(add-hook 'clojure-mode 'paredit-mode)                                                                                                                                          
+(add-hook 'cider-mode 'paredit-mode)                                                                                                                                            
+                                                                                                                                                                                
+(evil-mode 1)                                                                                                                                                                   
+(show-paren-mode 1)                                                                                                                                                             
+(column-number-mode 1)                                                                                                                                                          
+(line-number-mode 1)                                                                                                                                                            
+(rainbow-delimiters-mode 1)                                                                                                                                                     
+                                                                                                                                                                                
+(setq display-line-numbers-type 'relative)                                                                                                                                      
+(setq-default truncate-lines 1)                                                                                                                                                 
+(setq js-indent-level 2)                                                                                                                                                        
+(setq evil-want-C-u-scroll t) 
 
 ;; (setq tab-always-indent 'complete)
 
