f2afe98dd5a49d8a1208b4b9169314071ece72c1
gpapadok <38889721+gpapadok@users.noreply.github.com>
2023-02-18 15:43:46 +0200

    Update init.el

--- init.el
+++ init.el
@@ -3,19 +3,42 @@
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
 
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)                                                                                                                           
-(add-hook 'clojure-mode 'paredit-mode)                                                                                                                                          
-(add-hook 'cider-mode 'paredit-mode)                                                                                                                                            
-                                                                                                                                                                                
-(evil-mode 1)                                                                                                                                                                   
-(show-paren-mode 1)                                                                                                                                                             
-(column-number-mode 1)                                                                                                                                                          
-(line-number-mode 1)                                                                                                                                                            
-(rainbow-delimiters-mode 1)                                                                                                                                                     
-                                                                                                                                                                                
-(setq display-line-numbers-type 'relative)                                                                                                                                      
-(setq-default truncate-lines 1)                                                                                                                                                 
-(setq js-indent-level 2)                                                                                                                                                        
-(setq evil-want-C-u-scroll t) 
+;; SLIME
+(load (expand-file-name "~/.quicklisp/slime-helper.el"))
+(setq inferior-lisp-program "sbcl")
+
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+;; (add-hook 'prog-mode-hook 'paredit-mode)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
+;; (evil-mode 1)
+(menu-bar-mode 0)
+(show-paren-mode 1)
+(column-number-mode 1)
+(line-number-mode 1)
+(display-line-numbers-mode 1)
+
+;; (setq display-line-numbers-type 'relative)
+(setq-default truncate-lines 1)
+(setq js-indent-level 2)
+(setq sql-indent-level 4)
+;; (setq evil-want-C-u-scroll t)
 
 ;; (setq tab-always-indent 'complete)
+
+(load-theme 'cyberpunk t)
+
+;; (global-set-key (kbd "C-M-k") 'kill-whole-line)
+;; (global-set-key (kbd "C-M-k") 'kill-sexp)
+
+(defun kill-surrounding-sexp ()
+  (interactive)
+  (backward-up-list)
+  (kill-sexp))
+
+(defun yank-surrounding-sexp ()
+  (interactive)
+  (backward-up-list)
+  (let ((opoint (point)))
+    (forward-sexp 1)
+    (kill-ring-save opoint (point))))
