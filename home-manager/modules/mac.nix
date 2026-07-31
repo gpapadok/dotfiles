503f29e2673e9cbd326b3ad893d041c8e14842c7
gpapadok <giorgos.papadokostakis@proton.me>
2026-07-31 15:18:26 +0300

    nix: add brew autoupdate agent [ai]

--- home-manager/modules/mac.nix
+++ home-manager/modules/mac.nix
@@ -25,4 +25,23 @@
   programs.zsh.shellAliases = {
     spacemacs = "emacs --init-directory=~/.spacemacs.d";
   };
+
+  launchd.agents.brew-update = {
+    enable = true;
+    config = {
+      ProgramArguments = [
+        "${pkgs.writeShellScript "brew-update" ''
+          export PATH="/opt/homebrew/bin:$PATH"
+          brew update
+          brew upgrade
+          brew cleanup
+        ''}"
+      ];
+      StartCalendarInterval = [
+        { Weekday = 0; Hour = 9; Minute = 0; }
+      ];
+      StandardOutPath = "${config.home.homeDirectory}/Library/Logs/brew-update.log";
+      StandardErrorPath = "${config.home.homeDirectory}/Library/Logs/brew-update.log";
+    };
+  };
 }
